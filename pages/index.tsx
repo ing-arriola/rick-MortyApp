c203f7e243ad6a994745afce7a15d893bb47deb5
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-22 16:13:02 -0600

    Added grid to show characters and getting data from API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps } from 'next';
+import { Grid } from '@nextui-org/react';
+import { MainLayout } from '../components/Layouts';
+import { rickMortyApi } from '../api';
+import { CharacterListResponse, Character } from '../interfaces';
+import { CharacterCard } from '../components/ui';
 
-const HomePage: NextPage = () => {
+interface HomePageProps {
+  characters: Character[];
+}
+
+const HomePage: NextPage<HomePageProps> = ({ characters }) => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <MainLayout>
+      <Grid.Container gap={2} justify="flex-start">
+        {characters.map((character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+      </Grid.Container>
+    </MainLayout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await rickMortyApi.get<CharacterListResponse>('/character');
+
+  return {
+    props: {
+      characters: data.results,
+    },
+  };
 };
 
 export default HomePage;
