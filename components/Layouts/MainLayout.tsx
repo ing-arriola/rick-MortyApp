b1147488a859e5821205637b960199840a1d8703
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-23 14:25:32 -0600

    Added open metagraph info for sharing information in social media

--- components/Layouts/MainLayout.tsx
+++ components/Layouts/MainLayout.tsx
@@ -4,27 +4,27 @@ import { Navbar } from '../ui/Navbar';
 interface MainLayoutProps {
   children: JSX.Element | JSX.Element[];
   title?: string;
-  characterName?: string;
 }
 
-export const MainLayout = ({
-  children,
-  title,
-  characterName,
-}: MainLayoutProps) => {
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
+
+export const MainLayout = ({ children, title }: MainLayoutProps) => {
   return (
     <>
       <Head>
         <title>{title || 'Rick & Morty App'}</title>
         <meta name="author" content="Jaime Arriola" />
-        <meta
-          name="description"
-          content={`Information about ${characterName}`}
-        />
+        <meta name="description" content={`Information about ${title}`} />
         <meta
           name="keywords"
-          content={`${characterName}, Rick&Morty, Rick-and-Morty`}
+          content={`${title}, Rick&Morty, Rick-and-Morty`}
         />
+        <meta property="og:title" content={`Information about ${title}`} />
+        <meta
+          property="og:description"
+          content={`This is the page about ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.jpeg`} />
       </Head>
       <Navbar />
       <main style={{ padding: '0rem 1.5rem' }}>{children}</main>
